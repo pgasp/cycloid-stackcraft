2049bc688df50784eb3584d3faa3accf62b378f5
Pascal Gasp <pascal.gasp@gmail.com>
2020-02-12 16:22:02 +0000

    Update Service Catalog Terraform: 187-demostack

--- 187-demostack/terraform/module-187-demostack/config.tf
+++ 187-demostack/terraform/module-187-demostack/config.tf
@@ -12,6 +12,84 @@
   "region" = "eu-west-3"
 }
 
+"resource" "aws_db_instance" "mydatabase" {
+  "instance_class" = "mydatabase"
+
+  "tags" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+
+  "vpc_security_group_ids" = ["${aws_security_group.generated_sg_aws_db_instance_mydatabase.id}"]
+}
+
+"resource" "aws_instance" "serveur" {
+  "ami" = "ami-007fae589fdf6e955"
+
+  "instance_type" = "t2.micro"
+
+  "tags" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+
+  "vpc_security_group_ids" = ["${aws_security_group.generated_sg_aws_instance_serveur.id}"]
+}
+
+"resource" "aws_security_group" "generated_sg_aws_db_instance_mydatabase" {
+  "name_prefix" = "generated_sg_aws_db_instance_mydatabase"
+
+  "tags" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+}
+
+"resource" "aws_security_group" "generated_sg_aws_instance_serveur" {
+  "name_prefix" = "generated_sg_aws_instance_serveur"
+
+  "tags" = {
+    "cycloid.io" = "true"
+
+    "env" = "${var.env}"
+  }
+}
+
+"resource" "aws_security_group_rule" "rule_egress_generated_sg_aws_instance_serveur_to_generated_sg_aws_db_instance_mydatabase_3306_3306" {
+  "description" = "sql"
+
+  "from_port" = "3306"
+
+  "protocol" = "TCP"
+
+  "security_group_id" = "${aws_security_group.generated_sg_aws_instance_serveur.id}"
+
+  "source_security_group_id" = "${aws_security_group.generated_sg_aws_db_instance_mydatabase.id}"
+
+  "to_port" = "3306"
+
+  "type" = "egress"
+}
+
+"resource" "aws_security_group_rule" "rule_ingress_generated_sg_aws_instance_serveur_to_generated_sg_aws_db_instance_mydatabase_3306_3306" {
+  "description" = "sql"
+
+  "from_port" = "3306"
+
+  "protocol" = "TCP"
+
+  "security_group_id" = "${aws_security_group.generated_sg_aws_db_instance_mydatabase.id}"
+
+  "source_security_group_id" = "${aws_security_group.generated_sg_aws_instance_serveur.id}"
+
+  "to_port" = "3306"
+
+  "type" = "ingress"
+}
+
 "variable" "env" {
   "default" = "[PLACEHOLDER]"
 
